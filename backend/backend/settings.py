b663b7fc7d69d5d65a53e21a808fccb39d7520e2
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2022-12-20 15:08:53 +0700

    add limit and add data in crew

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE' : 100
+    'PAGE_SIZE' : 10
 }
 
 INSTALLED_APPS = [
--- backend/backend_api/views.py
+++ backend/backend_api/views.py
@@ -3,14 +3,17 @@ from backend_api.serializers import CourseSerializer, CrewSerializer
 from backend_api.models import Course, Crew
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.pagination import LimitOffsetPagination
 
 # Create your views here.
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = LimitOffsetPagination
 
 class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = LimitOffsetPagination
